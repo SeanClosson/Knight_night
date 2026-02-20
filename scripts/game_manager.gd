31deb32f273991987f6928d797a5cdad4083848a
SeanClosson <digisilk@yahoo.com>
2026-02-20 11:58:44 -0600

    HUD labels applied

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,16 @@
 extends Node
 
-var score = 0
+var score: int = 0
+
+@onready var player_hud: CanvasLayer = $PlayerHUD
 
 func add_points():
 	score += 1
-	print("score = " + str(score))
+	#print("score = " + str(score))
+	update_score(score)
+
+func update_score(score):
+	# print("func update score: " + str(player_hud.get_children()))
+	var a = player_hud.get_canvas()
+	print("canvas: " + str(a))
+	pass
